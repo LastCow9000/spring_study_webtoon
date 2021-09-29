3acbea44b4ea25ed4cafc784404df770a219043c
LastCow9000 <sys19912002@hanmail.net>
2021-09-29 23:59:37 +0900

    [chore] spring start

---
+++ src/main/java/com/study_webtoon/lastcow/LastcowApplication.java
@@ -0,0 +1,13 @@
+package com.study_webtoon.lastcow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastcowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastcowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study_webtoon/lastcow/LastcowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study_webtoon.lastcow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastcowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
